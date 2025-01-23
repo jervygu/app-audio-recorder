98dc037e960d9a2edfc98a3ed352e9576a99945f
Jervy Umandap <jervygu@gmail.com>
2025-01-23 21:12:33 +0800

    - Record and play audio

--- Recorder/Recorder/ViewController.swift
+++ Recorder/Recorder/ViewController.swift
@@ -6,25 +6,136 @@
 //
 
 import UIKit
+import AVFoundation
 
 class ViewController: UIViewController {
     
     @IBOutlet weak var recordButton: UIButton!
     @IBOutlet weak var playButton: UIButton!
+    
+    var recordingSession: AVAudioSession!
+    var audioRecorder: AVAudioRecorder!
+    var audioPlayer: AVAudioPlayer!
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
-    }
-    
-    
-    @IBAction func record(_ sender: Any) {
         
+        recordingSession = AVAudioSession.sharedInstance()
+        
+        do {
+            try recordingSession.setCategory(.playAndRecord, mode: .default)
+            try recordingSession.setActive(true)
+            recordingSession.requestRecordPermission { [unowned self] allowed in
+                DispatchQueue.main.async {
+                    if allowed {
+                        self.loadRecordingUI()
+                    } else {
+                        // failed to record
+                    }
+                }
+            }
+        } catch {
+            // failed to record!
+        }
     }
     
-    @IBAction func play(_ sender: Any) {
+    func loadRecordingUI() {
+        recordButton.isHidden = false
+        recordButton.setTitle("Tap to Record", for: .normal)
     }
     
+    
+    @IBAction func record(_ sender: UIButton) {
+        if audioRecorder == nil {
+            startRecording()
+        } else {
+            finishRecording(success: true)
+        }
+    }
+    
+    @IBAction func play(_ sender: UIButton) {
+        if audioPlayer == nil {
+            startPlayback()
+        } else {
+            finishPlayback()
+        }
+    }
+    
+    func getDocumentsDirectory() -> URL {
+        let paths = FileManager.default.urls(for: .documentDirectory, in: .userDomainMask)
+        return paths[0]
+    }
+    
+    // MARK: - Recording
 
+    func startRecording() {
+        let audioFilename = getDocumentsDirectory().appendingPathComponent("recording.m4a")
+        
+        let settings = [AVFormatIDKey: Int(kAudioFormatMPEG4AAC),
+                      AVSampleRateKey: 12000,
+                AVNumberOfChannelsKey: 1,
+             AVEncoderAudioQualityKey: AVAudioQuality.medium.rawValue]
+        
+        do {
+            audioRecorder = try AVAudioRecorder(url: audioFilename, settings: settings)
+            audioRecorder.delegate = self
+            audioRecorder.record()
+            
+            recordButton.setTitle("Tap to Stop", for: .normal)
+        } catch {
+            finishRecording(success: false)
+        }
+    }
+    
+    func finishRecording(success: Bool) {
+        audioRecorder.stop()
+        audioRecorder = nil
+        
+        if success {
+            recordButton.setTitle("Tap to Re-record", for: .normal)
+            playButton.setTitle("Play Your Recording", for: .normal)
+            playButton.isHidden = false
+        } else {
+            recordButton.setTitle("Tap to Record", for: .normal)
+            playButton.isHidden = true
+            // recording failed :(
+        }
+    }
+    
+    // MARK: - Playback
+    
+    func startPlayback() {
+        let audioFilename = getDocumentsDirectory().appendingPathComponent("recording.m4a")
+        do {
+            audioPlayer = try AVAudioPlayer(contentsOf: audioFilename)
+            audioPlayer.delegate = self
+            audioPlayer.play()
+            playButton.setTitle("Stop Playback", for: .normal)
+        } catch {
+            playButton.isHidden = true
+            // unable to play recording!
+        }
+    }
+    
+    func finishPlayback() {
+        audioPlayer = nil
+        playButton.setTitle("Play Your Recording", for: .normal)
+    }
+
+}
+
+extension ViewController: AVAudioRecorderDelegate {
+    func audioRecorderDidFinishRecording(_ recorder: AVAudioRecorder, successfully flag: Bool) {
+        if !flag {
+            finishRecording(success: false)
+        }
+    }
+}
+
+extension ViewController: AVAudioPlayerDelegate {
+    func audioPlayerDidFinishPlaying(_ player: AVAudioPlayer, successfully flag: Bool) {
+        finishPlayback()
+    }
 }
 
