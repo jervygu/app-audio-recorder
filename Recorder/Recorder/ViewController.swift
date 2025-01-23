e990ac6eecc4d21a778f61fd2ce2b16cea296d27
Jervy Umandap <jervygu@gmail.com>
2025-01-23 19:43:59 +0800

    - Buttons and outlets

--- Recorder/Recorder/ViewController.swift
+++ Recorder/Recorder/ViewController.swift
@@ -8,12 +8,23 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    @IBOutlet weak var recordButton: UIButton!
+    @IBOutlet weak var playButton: UIButton!
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
-
+    
+    
+    @IBAction func record(_ sender: Any) {
+        
+    }
+    
+    @IBAction func play(_ sender: Any) {
+    }
+    
 
 }
 
